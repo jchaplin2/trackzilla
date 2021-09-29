af2f4eb1a1da275b4c1a16363fea78a272692654
Jon Chaplin <jchaplin@gmail.com>
2021-09-29 06:56:46 -0400

    Add IntegrationTest for controller.

---
+++ src/it/java/com/trackzilla/config/DbConfig.java
@@ -0,0 +1,41 @@
+package com.trackzilla.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.test.context.ContextConfiguration;
+
+import javax.sql.DataSource;
+
+@ContextConfiguration()
+public class DbConfig {
+
+    @Autowired
+    Environment env;
+
+    @Bean
+    public DataSource dataSource() {
+        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setUrl(env.getProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
+
+        return dataSource;
+    }
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(DataSource ds) {
+        ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
+        resourceDatabasePopulator.addScript(new ClassPathResource("/schema-it.sql"));
+        resourceDatabasePopulator.addScript(new ClassPathResource("/data-it.sql"));
+        DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
+        dataSourceInitializer.setDataSource(ds);
+        dataSourceInitializer.setDatabasePopulator(resourceDatabasePopulator);
+        return dataSourceInitializer;
+    }
+}
---
+++ src/it/java/com/trackzilla/controller/TzaRestControllerIT.java
@@ -0,0 +1,63 @@
+package com.trackzilla.controller;
+
+import com.trackzilla.TrackzillaApplication;
+import com.trackzilla.config.DbConfig;
+import com.trackzilla.entity.Application;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@SpringBootTest(classes = {TrackzillaApplication.class, DbConfig.class},
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ActiveProfiles("it")
+public class TzaRestControllerIT {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void getAllApplications() throws Exception {
+        ResponseEntity<List> response =
+                this.restTemplate.getForEntity("http://localhost:" + port + "/trackzilla/applications/", List.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+    }
+
+    @Test
+    public void getAllTickets() throws Exception {
+        ResponseEntity<List> response =
+                this.restTemplate.getForEntity("http://localhost:" + port + "/trackzilla/tickets/", List.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+    }
+
+    @Test
+    public void getAllReleases() throws Exception {
+        ResponseEntity<List> response =
+                this.restTemplate.getForEntity("http://localhost:" + port + "/trackzilla/releases/", List.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+    }
+
+    @Test
+    public void getOneApplication() throws Exception {
+        ResponseEntity<Application> response =
+                this.restTemplate.getForEntity("http://localhost:" + port + "/trackzilla/application/1", Application.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+    }
+}
